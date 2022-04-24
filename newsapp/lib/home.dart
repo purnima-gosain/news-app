e7dc18b0a263c8a26c74aeeb88dad55f799a9d79
purnima-poo <np03a190173@herladcollege.edu.np>
2022-04-24 07:45:33 +0545

    fetched data from api

--- newsapp/lib/home.dart
+++ newsapp/lib/home.dart
@@ -1,15 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:newsapp/models/post.dart';
+import 'package:newsapp/services/remote_services.dart';
+import 'dart:convert';
+
+String? stringResponse;
+Map? mapResponse;
+Map? dataResponse;
+List? listResponse;
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future apiCall() async {
+    http.Response response;
+    response = await http.get(Uri.parse(
+        "https://newsapi.org/v2/everything?q=bitcoin&apiKey=d394960d6d364a9eacb07e25ed5d5e76"));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        //stringResponse = response.body;
+        mapResponse = json.decode(response.body);
+        listResponse = mapResponse!["articles"];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    apiCall();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Latest News"),
+          backgroundColor: Colors.teal,
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(10, 8.0, 8.0, 10),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Column(
+                  children: [
+                    Image.network(listResponse![index]["urlToImage"],
+                        height: 250, width: 380, fit: BoxFit.cover),
+                    ListTile(
+                      title: Text(
+                        listResponse![index]["title"].toString(),
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle:
+                          Text(listResponse![index]["description"].toString()),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          itemCount: listResponse == null ? 0 : listResponse?.length,
+        ));
   }
 }
--- newsapp/lib/splash.dart
+++ newsapp/lib/splash.dart
@@ -27,8 +27,66 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: ListView(
-      children: [Image.asset("assets/gimage.png")],
-    ));
+          children: [
+            Padding(
+                padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+                child: Image.asset("assets/news.jpg")),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Get the latest news",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "On your finger tip",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Click on skip to see the news",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 15,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 100,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(50, 60, 50, 0),
+                child: ElevatedButton(
+                  child: const Text("Skip >>"),
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color.fromARGB(220, 41, 167, 131),
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0)),
+                    // minimumSize: const Size.fromWidth(100),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: ((context) => HomeScreen())));
+                  },
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
